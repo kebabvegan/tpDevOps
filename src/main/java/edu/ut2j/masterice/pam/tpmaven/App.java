15a2df35697619f775956819f31cbf48741964fb
pablo-ignacio.mendez <pabloignaciomendezpoveda@gmail.com>
2019-10-24 15:39:32 +0200

    fist commit, tp maven

---
+++ src/main/java/edu/ut2j/masterice/pam/tpmaven/App.java
@@ -0,0 +1,13 @@
+package edu.ut2j.masterice.pam.tpmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
